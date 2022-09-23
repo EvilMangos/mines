b0516f5b42068cf409cb599387c0b27dd921ef2a
EvilMangos <jchamets@gmail.com>
2022-09-23 22:31:42 +0300

    M-102 init field

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ const Game = require('./components/game');
 
 const game = new Game();
 
-game.startGame({width: 10, height: 20, minesCount: 40});
+game.startGame({width: 3, height: 4, minesCount: 3});
