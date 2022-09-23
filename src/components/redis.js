169a9e81e674cf220822fe2c39b66753a2009d84
EvilMangos <jchamets@gmail.com>
2022-09-23 23:08:06 +0300

    M-103: fix redis

--- src/components/redis.js
+++ src/components/redis.js
@@ -3,7 +3,7 @@ const redis = require('redis');
 class Redis {
 	constructor() {
 		this.client = redis.createClient({
-			url: process.env.REDIS_HOST,
+			host: process.env.REDIS_HOST,
 			port: process.env.REDIS_PORT,
 		});
 	}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+require('dotenv').config({path: 'configs/local.env'});
+
 const Game = require('./components/game');
 
 const game = new Game();
